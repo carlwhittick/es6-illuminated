4936f7e58cb9c594e9edd15c7e78b7097d2ed9bf
Carl Whittick <carlwhittick@gmail.com>
2018-06-30 15:16:35 +0100

    Added comments to LineObject class

--- src/LineObject.js
+++ src/LineObject.js
@@ -1,11 +1,31 @@
 import PolygonObject from './PolygonObject.js'
 import Vec2 from './Vec2.js'
 
+/**
+ * An opaque line object.
+ */
 class LineObject extends PolygonObject {
+    /**
+     * Create a new instance of LineObject.
+     * @param {Object} options Options to be applied to the LineObject
+     */
     constructor(options) {
         super(options)
         const defaults = {
+            /**
+             * A vector that is the start point of the line.
+             * @property a
+             * @type Vec2
+             * @default Vec2()
+             */
             a: new Vec2(),
+
+            /**
+             * A vector that is the last point of the line.
+             * @property b
+             * @type Vec2
+             * @default Vec2()
+             */
             b: new Vec2()
         }
         this.options = Object.assign(defaults, this.options)
@@ -14,6 +34,9 @@ class LineObject extends PolygonObject {
         this.syncFromAB()
     }
 
+    /**
+     * Initializes the points defining this line based on its options.
+     */
     syncFromAb() {
         this.options.points = [this.options.a, this.options.b]
     }

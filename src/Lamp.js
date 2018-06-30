41b6ff01f2c35262f7f9eda813f9520bbdcff0a5
Carl Whittick <carlwhittick@gmail.com>
2018-06-30 14:50:27 +0100

    Added comments to the Lamp class

--- src/Lamp.js
+++ src/Lamp.js
@@ -2,21 +2,86 @@ import Vec2 from './Vec2'
 import Light from './Light.js'
 import { createCanvasAnd2dContext, getRGBA } from './helpers.js'
 
+/**
+ * A circular light rendered as a radial gradient.
+ */
 class Lamp extends Light {
+
+    /**
+     * Create new instance of Lamp
+     * @param {Object} options Options to be applied to Lamp
+     * @example
+     * new Lamp({
+     *     position: new Vec2(12, 34),
+     *     distance: 100,
+     *     diffuse: 0.8,
+     *     color: 'rgba(250, 220, 150, 0.8)',
+     *     radius: 0,
+     *     samples: 1,
+     *     angle: 0,
+     *     roughness: 0
+     * })
+     */
     constructor(options) {
         super()
         const defaults = {
+            /**
+             * The id of this light object.
+             * @property id
+             * @type Number
+             * @default 0
+             */
             id: 0,
+
+            /**
+             * The color emitted by the lamp. The color can be specified in any CSS format.
+             * @property color
+             * @type String
+             * @default 'rgba(250,220,150,0.8)'
+             */
             color: 'rgba(250,220,150,0.8)',
+
+            /**
+             * The size of the lamp. Bigger lamps cast smoother shadows.
+             * @property radius
+             * @type Number
+             * @default 0
+             */
             radius: 0,
+
+            /**
+             * The number of points which will be used for shadow projection. It defines
+             * the quality of the rendering.
+             * @property samples
+             * @type Number
+             * @default 1
+             */
             samples: 1,
+
+            /**
+             * The angle of the orientation of the lamp.
+             * @property angle
+             * @type Number
+             * @default 0
+             */
             angle: 0,
+
+            /**
+             * The roughness of the oriented effect.
+             * @property roughness
+             * @type Number
+             * @default 0
+             */
             roughness: 0
         }
         this.options = Object.assign(defaults, this.options)
         this.options = Object.assign(this.options, options)
     }
 
+    /**
+     * Generate a hash key representing this lamp.
+     * @return {String} A hash key.
+     */
     _getHashCache() {
         return [
             this.options.color,
@@ -29,6 +94,11 @@ class Lamp extends Light {
         ].toString()
     }
 
+    /**
+     * Return the center of this lamp.
+     * i.e. The position where the lamp intensity is the highest
+     * @return {Vec2} A new vector that represents the center of this lamp.
+     */
     center() {
         return new Vec2(
             (1 - Math.cos(this.options.angle) * this.options.roughness) * this.options.distance,
@@ -36,6 +106,11 @@ class Lamp extends Light {
         )
     }
 
+    /**
+     * Calculate the boundaries of this lamp based on its properties.
+     * @return {Object} An anonymous object with the properties topleft and bottomright representing
+     *                  the corners of the boundry.
+     */
     bounds() {
         var orientationCenter = new Vec2(
             Math.cos(this.options.angle), -Math.sin(this.options.angle)
@@ -52,6 +127,10 @@ class Lamp extends Light {
         }
     }
 
+    /**
+     * Render a mask representing the visibility. (Used by DarkMask.)
+     * @param {CanvasRenderContext2D} ctx The canvas contect to render the mask on.
+     */
     mask(ctx) {
         var c = this._getVisibleMaskCache()
         var orientationCenter = new Vec2(
@@ -63,6 +142,10 @@ class Lamp extends Light {
         )
     }
 
+    /**
+     * Renders this lamp's gradient onto a cached canvas at the given position.
+     * @param {Vec2} center The center position of the gradient to render.
+     */
     _getGradientCache(center) {
         var hashcode = this._getHashCache()
         if (this._cacheHashcode == hashcode) {
@@ -80,6 +163,10 @@ class Lamp extends Light {
         return this._gcache = cache
     }
 
+    /**
+     * Render the lamp onto the given context (without any shadows).
+     * @param {CanvasRenderContext2D} ctx The canvas context to render the light on.
+     */
     render(ctx) {
         var center = this.center()
         var c = this._getGradientCache(center)
